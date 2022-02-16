770c6ae9135f2a045b2f292b22db102a0a40c403
Shurna83 <42765739+Shurna83@users.noreply.github.com>
2022-02-16 23:54:11 +0100

    Parse json response

---
+++ src/api/apiClient.test.ts
@@ -0,0 +1,72 @@
+import { httpGet } from "../http/httpClient";
+import { isLeft, isRight, left, right } from "../utils/either";
+import { fetchQuestions } from "./apiClient";
+
+jest.mock("../http/httpClient");
+const mockHttpGet = httpGet as jest.MockedFunction<typeof httpGet>;
+
+afterEach(() => {
+  mockHttpGet.mockReset();
+});
+
+describe("fetchQuestions", () => {
+  test("should make proper GET request", async () => {
+    mockHttpGet.mockResolvedValueOnce(right("{}"));
+
+    await fetchQuestions();
+
+    expect(mockHttpGet).toBeCalledTimes(1);
+    expect(mockHttpGet).toBeCalledWith("data.json");
+  });
+
+  test("should parse response data", async () => {
+    const jsonQuestions = `[
+    {
+      "question": "q1",
+      "answers": ["a11", "a12", "a13"],
+      "correct": 0
+    },
+    {
+      "question": "q2",
+      "answers": ["a21", "a22", "a23"],
+      "correct": 2
+    }]`;
+    mockHttpGet.mockResolvedValueOnce(right(jsonQuestions));
+
+    const res = await fetchQuestions();
+
+    expect(isRight(res)).toBeTruthy();
+    expect(res.value).toStrictEqual([
+      {
+        question: "q1",
+        answers: ["a11", "a12", "a13"],
+        correct: 0,
+      },
+      {
+        question: "q2",
+        answers: ["a21", "a22", "a23"],
+        correct: 2,
+      },
+    ]);
+  });
+
+  test("when error parsing JSON, then should resolve with error", async () => {
+    const jsonQuestions = `] invalid json [`;
+    mockHttpGet.mockResolvedValueOnce(right(jsonQuestions));
+
+    const res = await fetchQuestions();
+
+    expect(isLeft(res)).toBeTruthy();
+    expect(res.value).toBe("Cannot understand server response");
+  });
+
+  test("when httpGet fails, then error is propagated", async () => {
+    const errMess = "An error";
+    mockHttpGet.mockResolvedValueOnce(left(errMess));
+
+    const res = await fetchQuestions();
+
+    expect(isLeft(res)).toBeTruthy();
+    expect(res.value).toBe(errMess);
+  });
+});
--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,5 +1,19 @@
 import { RecipeQuestion } from "../domain/RecipeQuestion";
+import { httpGet } from "../http/httpClient";
+import { Either, isLeft, left, right } from "../utils/either";
 
-export async function fetchQuestions(): Promise<RecipeQuestion[]> {
-  throw new Error("not implemented");
+export async function fetchQuestions(): Promise<
+  Either<string, RecipeQuestion[]>
+> {
+  const res = await httpGet<string>("data.json");
+  return isLeft(res) ? left(res.value) : parseQuestions(res.value);
+}
+
+function parseQuestions(json: string): Either<string, RecipeQuestion[]> {
+  try {
+    const questions: RecipeQuestion[] = JSON.parse(json);
+    return right(questions);
+  } catch (e) {
+    return left("Cannot understand server response");
+  }
 }
