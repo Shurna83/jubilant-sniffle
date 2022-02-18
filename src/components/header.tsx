74fcbbbe8ae2487883bda0d0a8ac9af39f94b7f0
Shurna83 <42765739+Shurna83@users.noreply.github.com>
2022-02-18 23:19:26 +0100

    Layout fixes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="vBox crossAxisCenter quizHeader">
+    <header className="vBox crossAxisCenter gap1 quizHeader">
       <h1>Test: come si fa la carbonara?</h1>
       <h2>Rispondi alle domande</h2>
     </header>
--- src/components/result.tsx
+++ src/components/result.tsx
@@ -6,12 +6,12 @@ export function Result({
   quizResult: { finalEvaluation, correctAnswers, totalQuestions },
 }: ResultProps): JSX.Element {
   return (
-    <>
+    <header className="vBox crossAxisCenter gap2 quizResult">
       <h4>
         Hai risposto correttamente a {correctAnswers}
         {correctAnswers === 1 ? " domanda" : " domande"} su {totalQuestions}
       </h4>
       <h1>{finalEvaluation}</h1>
-    </>
+    </header>
   );
 }
